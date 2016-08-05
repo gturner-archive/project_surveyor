519f31e6ea66f32179f98b8cb6dd179459f31070
Graham Turner <turnertgraham@gmail.com>
2016-08-05 15:00:37 -0400

    working on form

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,27 @@
 class SurveysController < ApplicationController
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
